9fcd6580ed3cfb049e13897bea16e219b2894d09
Luis <ljmora1@utpl.edu.ec>
2022-10-26 15:50:00 -0500

    Ejemplo IDE

---
+++ ejemplos-IDE/Ejemplo01/src/ejemplo01/Ejemplo02.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejemplo01;
+
+/**
+ *
+ * @author SALA I
+ */
+public class Ejemplo02 {
+    public static void main(String args[]){
+        
+        System.out.println("Nueva leyenda");
+    }
+}
---
+++ ejemplos-IDE/Ejemplo01/src/ejemplo01/Ejemplo03.java
@@ -0,0 +1,14 @@
+/*
+
+ */
+package ejemplo01;
+
+/**
+ *
+ * @author SALA I
+ */
+public class Ejemplo03 {
+    public static void main(String[] args) {
+        System.out.println("Leyenda 2");
+    }
+}
