e55095e5f6c79c4e5b9ce02aa4d8fa479db6ca24
Luis <ljmora1@utpl.edu.ec>
2022-10-28 11:22:47 -0500

    clase 3

--- ejemplos-IDE/Ejemplo03/src/ejemplo03/Ejemplo03.java
+++ ejemplos-IDE/Ejemplo03/src/ejemplo03/Ejemplo03.java
@@ -14,37 +14,34 @@ public class Ejemplo03 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String args[]) {
         // Tipos de dato en Java
-        
+
         // Tipo de datos Entero
-        
         int variable1;
         int variable2 = 10;
         int valorInicial = 18;
         int valor_inicial = 18;
         // int valor inicial = 18;
         int valor_inicial_2 = 18;
-        
+
         // Tipo de datos Double - Decimal
-        
         double variable3;
         double variable4 = 10.2;
-        
+
         // Tipo de datos String  - Cadena
-        
         String variable5;
         String variable6 = "Hola mundo";
-        
-        // Tipo de datos char  - un caractér
 
+        // Tipo de datos char  - un caractér
         char variable7;
         char variable8 = 'a';
-        
+
         // tipo de dato boolean - boleano
-        
         boolean variable9 = false;
         boolean variable10 = true;
+        boolean variable11 = false;
+        // boolean variable11 = false; // incorrecto
+        // variable10 = 10; // incorrecto
     }
-    
 }
--- ejemplos-IDE/Ejemplo04/src/ejemplo04/Ejemplo04.java
+++ ejemplos-IDE/Ejemplo04/src/ejemplo04/Ejemplo04.java
@@ -25,14 +25,14 @@ public class Ejemplo04 {
         int variable2 = 10 * 10;
         System.out.println(variable2);
         
+        
         System.out.println("----------------");
-        double variable3 = 10 * 10;
+        double variable3 = 10.2 * 10.9;
         System.out.println(variable3);
         
         System.out.println("----------------");
-        String variable4 = "10" + "10";
+        String variable4 = "10 " + "10";  // 10 10
         System.out.println(variable4);
-        
         
     }
     
